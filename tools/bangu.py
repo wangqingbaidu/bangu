ee9b9dc704131bc69801728689128aaaa387245b
Vlon Jang <wangqingbaidu@gmail.com>
2016-11-27 16:44:12 +0800

    change setup to bangu

--- tools/bangu.py
+++ tools/bangu.py
@@ -16,7 +16,7 @@ Note: Please keep the above information whenever or wherever the codes are used.
 '''
 import argparse, shutil, os
 parser = argparse.ArgumentParser(description='install bangu by root')
-parser.add_argument('opts', choices=['install', 'run'])
+parser.add_argument('opts', choices=['install', 'run', 'kill'])
 args = parser.parse_args()
 
 import time
@@ -25,11 +25,24 @@ if args.opts == 'install':
     if not os.path.exists('/usr/local/lib/python2.7/dist-packages/GetBanguHome.py'):
         shutil.copy('GetBanguHome.py', '/usr/local/lib/python2.7/dist-packages/')
     current_dir = os.getcwd()
-    sh = "#!/bin/sh\npython %s" %(current_dir + '/' + 'setup.py run &')
+    sh = """
+#!/bin/sh\npython
+case $1 in
+    start)
+        python {0} {1}
+        ;;
+    stop)
+        python {0} {2}
+        ;;
+*)
+echo "Usage: $0 (start|stop)"
+;;
+esac    
+    """.format(current_dir + '/' + 'setup.py ', 'run &', 'kill')
     bangu_auto = open('/etc/init.d/bangu', 'w')
     bangu_auto.write(sh)
     bangu_auto.close()
-    os.system('ln -s /etc/init.d/bangu /etc/rc.d/rc3.d/Sbangu')
+    os.system('ln -s /etc/init.d/bangu /etc/rc3.d/Sbangu')
     
     
 elif args.opts == 'run':
@@ -47,24 +60,12 @@ elif args.opts == 'run':
     
     while True:
         time.sleep(901022)
-elif args.opts == 'auto':
-    if not os.path.exists('/usr/local/lib/python2.7/dist-packages/GetBanguHome.py'):
-        shutil.copy('GetBanguHome.py', '/usr/local/lib/python2.7/dist-packages/')
-    
-    import GetBanguHome, thread
-    import os
-    
-    
-    from Controller.UpdateWeather import ThreadUpdateWeather2DB
-    from View.Hardware.LED_WeatherForecast import ThreadWeatherLEDFlicker
-    from utils.ReadConfig import configurations
-    
-    thread.start_new_thread(ThreadUpdateWeather2DB, (600,))
-    thread.start_new_thread(ThreadWeatherLEDFlicker, tuple())
-    
-    while True:
-        time.sleep(901022)
-    
+elif args.opts == 'kill':
+    res = os.popen('ps -ef|grep bangu').readlines()
+    for item in res[:-1]:
+        pid = item.split()[1]
+        print item
+        os.system('kill -9 %s', pid)
     
     
 
