f95fd739033a114d459648f0b60b5f9e4f539d41
Vlon Jang <wangqingbaidu@gmail.com>
2016-11-27 12:36:26 +0800

    get bangu home

--- Controller/UpdateWeather.py
+++ Controller/UpdateWeather.py
@@ -14,10 +14,7 @@ Contact Info: you can send an email to 564326047@qq.com(Vlon)
 
 Note: Please keep the above information whenever or wherever the codes are used.
 '''
-import sys, os
-sys.path.append(os.getenv('BANGUHOME', '~/bangu'))
-reload(sys)
-
+import GetBanguHome
 import urllib, urllib2, json
 from utils.ReadConfig import BanguConfig
 from datetime import datetime
---
+++ GetBanguHome.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*- 
+'''
+CreateBanguHome is a part of the project bangu.
+bangu is an open-source project which follows MVC design pattern mainly based on python.
+
+Copyright (C) 2014 - 2016, Vlon Jang(WeChat:wangqingbaidu)
+Institute of Computing Technology, Chinese Academy of Sciences, Beijing, China.
+
+The codes are mainly developed by Zhiwei Zhang.
+As an open-source project, your can use or modify it as you want.
+
+Contact Info: you can send an email to 564326047@qq.com(Vlon) 
+  or visit my website www.wangqingbaidu.cn
+
+Note: Please keep the above information whenever or wherever the codes are used.
+'''
+
+import sys, os
+current_path = os.getcwd()
+while True:
+    items = os.listdir(current_path)
+    if 'Model' in items and 'View' in items and 'Controller' in items:
+        break
+    else:
+        current_path = os.path.dirname(current_path)
+
+print current_path
+sys.path.append(current_path)
+reload(sys)
+
