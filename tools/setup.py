c83c0dfd0889d37a2187ace8859ae9dea75464bc
Vlon Jang <wangqingbaidu@gmail.com>
2016-11-27 16:21:59 +0800

    bangu auto run

--- tools/setup.py
+++ tools/setup.py
@@ -16,7 +16,7 @@ Note: Please keep the above information whenever or wherever the codes are used.
 '''
 import argparse, shutil, os
 parser = argparse.ArgumentParser(description='install bangu by root')
-parser.add_argument('opts', choices=['install', 'test', 'auto'])
+parser.add_argument('opts', choices=['install', 'run'])
 args = parser.parse_args()
 
 import time
@@ -24,6 +24,13 @@ import time
 if args.opts == 'install':
     if not os.path.exists('/usr/local/lib/python2.7/dist-packages/GetBanguHome.py'):
         shutil.copy('GetBanguHome.py', '/usr/local/lib/python2.7/dist-packages/')
+    current_dir = os.getcwd()
+    sh = "#!/bin/sh\npython %s" %(current_dir + '/' + 'setup.py run &')
+    bangu_auto = open('/etc/init.d/bangu', 'w')
+    bangu_auto.write(sh)
+    bangu_auto.close()
+    os.system('ln -s /etc/init.d/bangu /etc/rc.d/rc3.d/Sbangu')
+    
 elif args.opts == 'test':
     if not os.path.exists('/usr/local/lib/python2.7/dist-packages/GetBanguHome.py'):
         shutil.copy('GetBanguHome.py', '/usr/local/lib/python2.7/dist-packages/')
@@ -38,7 +45,24 @@ elif args.opts == 'test':
     thread.start_new_thread(ThreadWeatherLEDFlicker, tuple())
     
     while True:
-        time.sleep(60000)
+        time.sleep(901022)
+elif args.opts == 'auto':
+    if not os.path.exists('/usr/local/lib/python2.7/dist-packages/GetBanguHome.py'):
+        shutil.copy('GetBanguHome.py', '/usr/local/lib/python2.7/dist-packages/')
+    
+    import GetBanguHome, thread
+    import os
+    
+    
+    from Controller.UpdateWeather import ThreadUpdateWeather2DB
+    from View.Hardware.LED_WeatherForecast import ThreadWeatherLEDFlicker
+    from utils.ReadConfig import configurations
+    
+    thread.start_new_thread(ThreadUpdateWeather2DB, (600,))
+    thread.start_new_thread(ThreadWeatherLEDFlicker, tuple())
+    
+    while True:
+        time.sleep(901022)
     
     
     
