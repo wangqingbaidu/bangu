742c0035284909144fd30ca8d598b235238c7dca
Vlon Jang <wangqingbaidu@gmail.com>
2017-01-09 10:03:02 +0800

    merge time and fix 12:00 push message

--- utils/Timer.py
+++ utils/Timer.py
@@ -14,9 +14,28 @@ Contact Info: you can send an email to 564326047@qq.com(Vlon)
 
 Note: Please keep the above information whenever or wherever the codes are used.
 '''
-from Seconds2When import getSecond2Datetime, getSecond2When
-import threading, time
+import threading, time, datetime
 from termcolor import cprint
+
+def getSecond2When(year = None, month = None, day = None, hour = None, minute = None, second = None):
+    #If there exists a bug while converting, 0 will be returned. 
+    try:
+        now = datetime.now()
+        to = datetime(year if year != None else now.year, 
+                      month if month != None else now.month, 
+                      day if day != None else now.day, 
+                      hour if hour != None else 0,
+                      minute if minute != None else 0,
+                      second if second != None else 0)
+        
+        return (to - now).seconds + 1 if to > now else 0
+    except:
+        return 0
+    
+def getSecond2Datetime(d):
+    #If there exists a bug while converting, 0 will be returned. 
+    return getSecond2When(d.year, d.month, d.day, d.hour, d.minute, d.second)
+
 class Timer:
     def __init__(self, d, func, args={}, start_type='every'):
         self.func = func
@@ -37,9 +56,11 @@ class Timer:
             while True:
                 st = getSecond2When(hour = self.d.hour, minute=self.d.minute, second=self.d.second)
 #                 print st
-                time.sleep(st if st else getSecond2When(hour = 23, minute=59, second=59) + 1)
-                threading.Thread(target=self.func, kwargs=self.args).start()
+                if st == 0:                    
+                    time.sleep(getSecond2When(hour = 23, minute=59, second=59) + 1)
+                else:
+                    threading.Thread(target=self.func, kwargs=self.args).start()
             
 
 if __name__ == '__main__':
-    pass
+    print getSecond2When(hour= -23, minute=10)
